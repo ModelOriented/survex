18aa86e2dfd7f4027e06a34155a6eb7e97b1621b
Mikołaj Spytek <mikolajspytek@gmail.com>
2022-09-26 15:21:23 +0200

    Escaping brackets to fix check warning

--- R/metrics.R
+++ R/metrics.R
@@ -11,8 +11,8 @@
 #' @return numeric from 0 to 1, higher values indicate better performance
 #'
 #' @section References:
-#' - [[1] Harrell Jr, Frank E., et al. "Regression modelling strategies for improved prognostic prediction." Statistics in medicine 3.2 (1984): 143-152.](https://onlinelibrary.wiley.com/doi/abs/10.1002/sim.4780030207)
-#' - [[2] pysurvival documentation entry](https://square.github.io/pysurvival/metrics/c_index.html)
+#' - [\[1\] Harrell Jr, Frank E., et al. "Regression modelling strategies for improved prognostic prediction." Statistics in medicine 3.2 (1984): 143-152.](https://onlinelibrary.wiley.com/doi/abs/10.1002/sim.4780030207)
+#' - [\[2\] pysurvival documentation entry](https://square.github.io/pysurvival/metrics/c_index.html)
 #'
 #' @rdname c_index
 #' @seealso [loss_one_minus_c_index()]
@@ -63,8 +63,8 @@ attr(c_index, "loss_name") <- "C-index"
 #' @return numeric from 0 to 1, lower values indicate better performance
 #'
 #' @section References:
-#' - [[1] Harrell Jr, Frank E., et al. "Regression modelling strategies for improved prognostic prediction." Statistics in medicine 3.2 (1984): 143-152.](https://onlinelibrary.wiley.com/doi/abs/10.1002/sim.4780030207)
-#' - [[2] pysurvival documentation entry](https://square.github.io/pysurvival/metrics/c_index.html)
+#' - [\[1\] Harrell Jr, Frank E., et al. "Regression modelling strategies for improved prognostic prediction." Statistics in medicine 3.2 (1984): 143-152.](https://onlinelibrary.wiley.com/doi/abs/10.1002/sim.4780030207)
+#' - [\[2\] pysurvival documentation entry](https://square.github.io/pysurvival/metrics/c_index.html)
 #'
 #' @rdname loss_one_minus_c_index
 #' @seealso [c_index()]
@@ -106,8 +106,8 @@ attr(loss_one_minus_c_index, "loss_name") <- "One minus C-Index"
 #' @return numeric from 0 to 1, lower scores are better (brier score of 0.25 represents a model which returns always returns 0.5 as the predicted survival function)
 #'
 #' @section References:
-#' - [[1] Brier, Glenn W. "Verification of forecasts expressed in terms of probability." Monthly weather review 78.1 (1950): 1-3.](https://journals.ametsoc.org/view/journals/mwre/78/1/1520-0493_1950_078_0001_vofeit_2_0_co_2.xml)
-#' - [[2] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.brier_score.html)
+#' - [\[1\] Brier, Glenn W. "Verification of forecasts expressed in terms of probability." Monthly weather review 78.1 (1950): 1-3.](https://journals.ametsoc.org/view/journals/mwre/78/1/1520-0493_1950_078_0001_vofeit_2_0_co_2.xml)
+#' - [\[2\] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.brier_score.html)
 #'
 #' @rdname brier_score
 #' @seealso [cd_auc()]
@@ -167,8 +167,8 @@ attr(loss_brier_score, "loss_name") <- "Brier score"
 #' Calculate Cumulative/Dynamic AUC
 #'
 #' This function calculates the Cumulative/Dynamic AUC metric for a survival model. It is done using the
-#' estimator proposed proposed by Uno et al. [[1]](https://www.jstor.org/stable/27639883#metadata_info_tab_contents),
-#' and Hung and Chang [[2]](https://www.jstor.org/stable/41000414#metadata_info_tab_contents).
+#' estimator proposed proposed by Uno et al. [\[1\]](https://www.jstor.org/stable/27639883#metadata_info_tab_contents),
+#' and Hung and Chang [\[2\]](https://www.jstor.org/stable/41000414#metadata_info_tab_contents).
 #'
 #' C/D AUC is an extension of the AUC metric known from classification models.
 #' Its values represent the model's performance at specific time points.
@@ -182,9 +182,9 @@ attr(loss_brier_score, "loss_name") <- "Brier score"
 #' @return a numeric vector of length equal to the length of the times vector, each value (from the range from 0 to 1) represents the AUC metric at a specific time point, with higher values indicating better performance.
 #'
 #' @section References:
-#' - [[1] Uno, Hajime, et al. "Evaluating prediction rules for t-year survivors with censored regression models." Journal of the American Statistical Association 102.478 (2007): 527-537.](https://www.jstor.org/stable/27639883#metadata_info_tab_contents)
-#' - [[2] Hung, Hung, and Chin‐tsang Chiang. "Optimal composite markers for time‐dependent receiver operating characteristic curves with censored survival data." Scandinavian journal of statistics 37.4 (2010): 664-679.](https://www.jstor.org/stable/41000414#metadata_info_tab_contents)
-#' - [[3] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.cumulative_dynamic_auc.html)
+#' - [\[1\] Uno, Hajime, et al. "Evaluating prediction rules for t-year survivors with censored regression models." Journal of the American Statistical Association 102.478 (2007): 527-537.](https://www.jstor.org/stable/27639883#metadata_info_tab_contents)
+#' - [\[2\] Hung, Hung, and Chin‐tsang Chiang. "Optimal composite markers for time‐dependent receiver operating characteristic curves with censored survival data." Scandinavian journal of statistics 37.4 (2010): 664-679.](https://www.jstor.org/stable/41000414#metadata_info_tab_contents)
+#' - [\[3\] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.cumulative_dynamic_auc.html)
 #'
 #' @rdname cd_auc
 #' @seealso [loss_one_minus_cd_auc()] [integrated_cd_auc()] [brier_score()]
@@ -249,9 +249,9 @@ attr(cd_auc, "loss_name") <- "C/D AUC"
 #' @return a numeric vector of length equal to the length of the times vector, each value (from the range from 0 to 1) represents 1 - AUC metric at a specific time point, with lower values indicating better performance.
 #'
 #' #' @section References:
-#' - [[1] Uno, Hajime, et al. "Evaluating prediction rules for t-year survivors with censored regression models." Journal of the American Statistical Association 102.478 (2007): 527-537.](https://www.jstor.org/stable/27639883#metadata_info_tab_contents)
-#' - [[2] Hung, Hung, and Chin‐tsang Chiang. "Optimal composite markers for time‐dependent receiver operating characteristic curves with censored survival data." Scandinavian journal of statistics 37.4 (2010): 664-679.](https://www.jstor.org/stable/41000414#metadata_info_tab_contents)
-#' - [[3] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.cumulative_dynamic_auc.html)
+#' - [\[1\] Uno, Hajime, et al. "Evaluating prediction rules for t-year survivors with censored regression models." Journal of the American Statistical Association 102.478 (2007): 527-537.](https://www.jstor.org/stable/27639883#metadata_info_tab_contents)
+#' - [\[2\] Hung, Hung, and Chin‐tsang Chiang. "Optimal composite markers for time‐dependent receiver operating characteristic curves with censored survival data." Scandinavian journal of statistics 37.4 (2010): 664-679.](https://www.jstor.org/stable/41000414#metadata_info_tab_contents)
+#' - [\[3\] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.cumulative_dynamic_auc.html)
 #'
 #' @rdname loss_one_minus_cd_auc
 #' @seealso [cd_auc()]
@@ -290,9 +290,9 @@ attr(loss_one_minus_cd_auc, "loss_name") <- "One minus C/D AUC"
 #' @return numeric from 0 to 1, higher values indicate better performance
 #'
 #' #' @section References:
-#' - [[1] Uno, Hajime, et al. "Evaluating prediction rules for t-year survivors with censored regression models." Journal of the American Statistical Association 102.478 (2007): 527-537.](https://www.jstor.org/stable/27639883#metadata_info_tab_contents)
-#' - [[2] Hung, Hung, and Chin‐tsang Chiang. "Optimal composite markers for time‐dependent receiver operating characteristic curves with censored survival data." Scandinavian journal of statistics 37.4 (2010): 664-679.](https://www.jstor.org/stable/41000414#metadata_info_tab_contents)
-#' - [[3] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.cumulative_dynamic_auc.html)
+#' - [\[1\] Uno, Hajime, et al. "Evaluating prediction rules for t-year survivors with censored regression models." Journal of the American Statistical Association 102.478 (2007): 527-537.](https://www.jstor.org/stable/27639883#metadata_info_tab_contents)
+#' - [\[2\] Hung, Hung, and Chin‐tsang Chiang. "Optimal composite markers for time‐dependent receiver operating characteristic curves with censored survival data." Scandinavian journal of statistics 37.4 (2010): 664-679.](https://www.jstor.org/stable/41000414#metadata_info_tab_contents)
+#' - [\[3\] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.cumulative_dynamic_auc.html)
 #'
 #' @rdname integrated_cd_auc
 #' @seealso [cd_auc()] [loss_one_minus_cd_auc()]
@@ -345,9 +345,9 @@ attr(integrated_cd_auc, "loss_name") <- "integrated C/D AUC"
 #' @return numeric from 0 to 1, lower values indicate better performance
 #'
 #' #' @section References:
-#' - [[1] Uno, Hajime, et al. "Evaluating prediction rules for t-year survivors with censored regression models." Journal of the American Statistical Association 102.478 (2007): 527-537.](https://www.jstor.org/stable/27639883#metadata_info_tab_contents)
-#' - [[2] Hung, Hung, and Chin‐tsang Chiang. "Optimal composite markers for time‐dependent receiver operating characteristic curves with censored survival data." Scandinavian journal of statistics 37.4 (2010): 664-679.](https://www.jstor.org/stable/41000414#metadata_info_tab_contents)
-#' - [[3] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.cumulative_dynamic_auc.html)
+#' - [\[1\] Uno, Hajime, et al. "Evaluating prediction rules for t-year survivors with censored regression models." Journal of the American Statistical Association 102.478 (2007): 527-537.](https://www.jstor.org/stable/27639883#metadata_info_tab_contents)
+#' - [\[2\] Hung, Hung, and Chin‐tsang Chiang. "Optimal composite markers for time‐dependent receiver operating characteristic curves with censored survival data." Scandinavian journal of statistics 37.4 (2010): 664-679.](https://www.jstor.org/stable/41000414#metadata_info_tab_contents)
+#' - [\[3\] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.cumulative_dynamic_auc.html)
 #'
 #' @rdname loss_one_minus_integrated_cd_auc
 #' @seealso [integrated_cd_auc()] [cd_auc()] [loss_one_minus_cd_auc()]
@@ -394,8 +394,8 @@ attr(loss_one_minus_integrated_cd_auc, "loss_name") <- "One minus integrated C/D
 #' @return numeric from 0 to 1, lower values indicate better performance
 #'
 #' @section References:
-#' - [[1] Brier, Glenn W. "Verification of forecasts expressed in terms of probability." Monthly weather review 78.1 (1950): 1-3.](https://journals.ametsoc.org/view/journals/mwre/78/1/1520-0493_1950_078_0001_vofeit_2_0_co_2.xml)
-#' - [[2] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.brier_score.html)
+#' - [\[1\] Brier, Glenn W. "Verification of forecasts expressed in terms of probability." Monthly weather review 78.1 (1950): 1-3.](https://journals.ametsoc.org/view/journals/mwre/78/1/1520-0493_1950_078_0001_vofeit_2_0_co_2.xml)
+#' - [\[2\] scikit-survival documentation entry](https://scikit-survival.readthedocs.io/en/stable/api/generated/sksurv.metrics.brier_score.html)
 #'
 #' @rdname integrated_brier_score
 #' @seealso [brier_score()] [integrated_cd_auc()] [loss_one_minus_integrated_cd_auc()]
