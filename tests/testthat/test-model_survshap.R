2728dbb992be95c6e59825d5328b4aea81abb214
krzyzinskim <mateusz_krzyzinski@wp.pl>
2023-08-28 14:05:11 +0200

    add new model_survshap plots

--- tests/testthat/test-model_survshap.R
+++ tests/testthat/test-model_survshap.R
@@ -25,6 +25,9 @@ test_that("global survshap explanations with kernelshap work for ranger, using n
     plot(ranger_global_survshap, geom = "beeswarm")
     plot(ranger_global_survshap, geom = "profile", variable = "karno", color_variable = "celltype")
     plot(ranger_global_survshap, geom = "profile", variable = "karno", color_variable = "age")
+    plot(ranger_global_survshap, geom = "curves", variable = "karno")
+    plot(ranger_global_survshap, geom = "curves", variable = "celltype")
+    plot(ranger_global_survshap, geom = "curves", variable = "karno", boxplot = TRUE)
     expect_error(plot(ranger_global_survshap, geom = "nonexistent"))
 
     single_survshap <- extract_predict_survshap(ranger_global_survshap, 5)
@@ -49,6 +52,9 @@ test_that("global survshap explanations with kernelshap work for coxph, using ex
     plot(cph_global_survshap, geom = "beeswarm")
     plot(cph_global_survshap, geom = "profile", variable = "karno", color_variable = "celltype")
     plot(cph_global_survshap, geom = "profile", variable = "karno", color_variable = "age")
+    plot(cph_global_survshap, geom = "curves", variable = "karno")
+    plot(cph_global_survshap, geom = "curves", variable = "celltype")
+    plot(cph_global_survshap, geom = "curves", variable = "karno", boxplot = TRUE)
 
     expect_s3_class(cph_global_survshap, c("aggregated_surv_shap", "surv_shap"))
     expect_equal(length(cph_global_survshap$eval_times), length(cph_exp$times))
